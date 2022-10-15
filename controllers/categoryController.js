14b833969478a4b6dd302bf7a136dd7f37fdf3a7
Teo Lunas <rodrigo.lunasp@gmail.com>
2022-10-15 20:54:39 -0300

    base de archivo

---
+++ controllers/categoryController.js
@@ -0,0 +1,9 @@
+export default class CategoryController {
+    async getAllCategory() {
+
+    }
+
+    async getOneCategory() {
+        
+    }
+}
---
+++ controllers/productController.js
@@ -0,0 +1,9 @@
+export default class ProductController {
+    async getAllProducts () {
+
+    }
+
+    async getOnProducts(){
+        
+    }
+}
