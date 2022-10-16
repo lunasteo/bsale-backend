89acb34128db7947b9cf3604319f94ddac3a25d2
Teo Lunas <rodrigo.lunasp@gmail.com>
2022-10-16 01:34:37 -0300

    Se agrega validacion para racibir solo numeros en endpoint

--- routes/categoryRouter.js
+++ routes/categoryRouter.js
@@ -1,9 +1,10 @@
 import app from 'express';
 
 import CategoryController from '../controllers/categoryController.js';
+import validateNum from '../validators/category.js';
 const routes = app.Router();
 
 routes.get('/', new CategoryController().getAllCategories);
-routes.get('/:id', new CategoryController().getOneCategory);
+routes.get('/:id', validateNum, new CategoryController().getOneCategory);
 
 export default routes;
